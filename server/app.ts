0b4cb7a06186eff1cf409fd8e20a3509cf5f68fa
mhenn <Mikeianhenning93@gmail.com>
2024-01-20 21:48:38 +0100

    started stuff

---
+++ server/app.ts
@@ -0,0 +1,43 @@
+// app.ts
+import { Application, Router, walk } from "./deps.ts";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (context) => {
+  context.response.body = "Hello Deno!";
+});
+
+router.get("/pdf", async (context) => {
+  const files: string[] = [];
+  for await (const entry of walk("./res", {
+    includeDirs: false,
+    includeFiles: true,
+  })) {
+    files.push(entry.path.split("/").pop() as string);
+  }
+  context.response.body = files;
+});
+
+router.get("/pdf/:name", async (context) => {
+  for await (const entry of walk("./res", {
+    includeDirs: false,
+    includeFiles: true,
+  })) {
+    if (entry.path.includes(context.params.name)) {
+      const fileContent = await Deno.readFile(entry.path);
+      context.response.headers.set("Content-Type", "application/pdf");
+      context.response.headers.set(
+        "Content-Disposition",
+        `attachment; filename="${entry.path.split("/").pop()}"`
+      );
+      context.response.body = fileContent;
+      break;
+    }
+  }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
---
+++ server/deps.ts
@@ -0,0 +1,3 @@
+// deps.ts
+export { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
+export { walk } from "https://deno.land/std/fs/mod.ts";
